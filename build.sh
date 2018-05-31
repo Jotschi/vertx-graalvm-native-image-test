a6a19f7dc34489023e6a041117cb484b86e9db67
Johannes Schüth <git@jotschi.de>
2018-05-31 17:29:37 +0200

    Replace patches with a mock class

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ cd $PROJECT_DIR
 
 mvn clean package
 
-rm vertx-graalvm-native-image-test-0.0.1-SNAPSHOT 
+rm vertx-graal*
 $GRAALVMDIR/bin/native-image \
  --verbose \
  --no-server \
- -H:ReflectionConfigurationFiles=./reflectconfigs/netty \
  -Dio.netty.noUnsafe=true  \
+ -H:Name=hello-world \
+ -H:ReflectionConfigurationFiles=./reflectconfigs/netty \
  -H:+ReportUnsupportedElementsAtRuntime \
  -Dfile.encoding=UTF-8 \
  -jar target/vertx-graalvm-native-image-test-0.0.1-SNAPSHOT.jar
